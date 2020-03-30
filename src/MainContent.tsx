8aefbb13d3ed1f85c2d903bb1ef8fc38f313b01e
punitda <punitdama@gmail.com>
2020-03-30 13:00:33 +0530

    Move RecentProjects into separate component

--- src/MainContent.tsx
+++ src/MainContent.tsx
@@ -19,48 +19,56 @@ export default function MainContent() {
             <SearchIcon className="w-4 h-4 text-gray-500" />
           </span>
         </div>
-        <div className="w-full flex items-center justify-around flex-wrap">
-          <h2 className="flex-1 text-2xl font-semibold text-gray-800 my-6">
-            Recently Used
-          </h2>
-          <div className="mx-auto">
-            <GridIcon className="inline w-6 h-6 text-gray-700" />
-            <ListIcon className="inline ml-1 w-6 h-6 text-gray-500" />
-          </div>
-        </div>
-        <ul className="grid grid-cols-3 xl:grid-cols-4 gap-3">
-          {recentlyUsedProjects.map(project => {
-            return (
-              <div className="h-32 xl:h-40 p-4 relative flex flex-col justify-around rounded-lg bg-white shadow-lg">
-                <div className="flex items-start">
-                  <FileIcon className="w-8 h-8 text-orange-500" />
-                  <div className="ml-12 flex items-center">
-                    {project.images.map(imageUrl => (
-                      <span className="-ml-2 rounded-full border-2 border-white">
-                        <img
-                          className="h-6 w-6 rounded-full object-cover"
-                          src={imageUrl}
-                          alt="profile photos"
-                        />
-                      </span>
-                    ))}
-                  </div>
-                </div>
-                <div className="mt-auto">
-                  <h3 className="text-sm font-semibold text-gray-700">
-                    {project.name}
-                  </h3>
-                  <span className="text-xs text-gray-500">
-                    {project.createdAt}
-                  </span>
-                </div>
-                <VerticalDotsIcons className="absolute top-0 right-0 mt-2 mr-1 w-4 h-4 text-gray-500" />
-              </div>
-            );
-          })}
-        </ul>
+        <RecentProjects />
       </div>
     </div>
+  );
+}
+
+function RecentProjects() {
+  return (
+    <>
+      <div className="w-full flex items-center justify-around flex-wrap">
+        <h2 className="flex-1 text-2xl font-semibold text-gray-800 my-6">
+          Recently Used
+        </h2>
+        <div className="mx-auto">
+          <GridIcon className="inline w-6 h-6 text-gray-700" />
+          <ListIcon className="inline ml-1 w-6 h-6 text-gray-500" />
+        </div>
+      </div>
+      <ul className="grid grid-cols-3 xl:grid-cols-4 gap-3">
+        {recentlyUsedProjects.map(project => {
+          return (
+            <div className="h-32 xl:h-40 p-4 relative flex flex-col justify-around rounded-lg bg-white shadow-lg">
+              <div className="flex items-start">
+                <FileIcon className="w-8 h-8 text-orange-500" />
+                <div className="ml-12 flex items-center">
+                  {project.images.map(imageUrl => (
+                    <span className="-ml-2 rounded-full border-2 border-white">
+                      <img
+                        className="h-6 w-6 rounded-full object-cover"
+                        src={imageUrl}
+                        alt="profile photos"
+                      />
+                    </span>
+                  ))}
+                </div>
+              </div>
+              <div className="mt-auto">
+                <h3 className="text-sm font-semibold text-gray-700">
+                  {project.name}
+                </h3>
+                <span className="text-xs text-gray-500">
+                  {project.createdAt}
+                </span>
+              </div>
+              <VerticalDotsIcons className="absolute top-0 right-0 mt-2 mr-1 w-4 h-4 text-gray-500" />
+            </div>
+          );
+        })}
+      </ul>
+    </>
   );
 }
 
